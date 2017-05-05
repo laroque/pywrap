3f12d1308e24bec0317f9d9dead0a1bba8b846e1
Ben LaRoque <benjamin.laroque@gmail.com>
2017-05-04 17:09:48 -0700

    fails

--- src/data_user.h
+++ src/data_user.h
@@ -2,6 +2,7 @@
 #define TALKUSER_H_
 
 #include <string>
+#include <vector>
 #include "talk_data.h"
 
 class TalkUser
@@ -11,8 +12,14 @@ class TalkUser
         virtual ~TalkUser();
         TalkData GetData();
         void SetData(TalkData value);
+
+        template< typename T >
+        std::vector<T> FooMethod();
     private:
         TalkData fData;
 };
+
+template< typename T >
+std::vector<T> TalkUser::FooMethod(){ return new std::vector<T>;}
 
 #endif //TALKUSER_H_
--- src/data_userPy.h
+++ src/data_userPy.h
@@ -3,12 +3,15 @@
 
 #include "data_user.h"
 #include <boost/python.hpp>
+#include <boost/python/overloads.hpp>
+
 
 void export_data_user()
 {
     using namespace boost::python;
     class_< TalkUser >("TalkUser", "some user class")
         .add_property("Data", &TalkUser::GetData, &TalkUser::SetData, "data docstring")
+        .def("FooMethod", &TalkUser::FooMethod)//, return_value_policy<manage_new_object>(), "docstring")
     ;
 }
 
